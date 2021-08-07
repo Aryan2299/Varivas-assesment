9ae260ad89309bfe43e401e51d97710d39f33d5f
Aryan Sharma <aryan@Aryans-MacBook-Pro.local>
2021-08-07 20:16:47 +0530

    enabled cookie sessions

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
 const keys = require("./config/keys");
+const passport = require("passport");
+const { initialize, session } = require("passport");
 
 require("./models/User");
 require("./services/passport");
@@ -10,6 +13,15 @@ mongoose.connect(keys.mongoURI);
 const app = express();
 
 app.use(express.json());
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
+);
+
+app.use(passport.initialize());
+passport.use(passport.session());
 
 require("./routes/auth")(app);
 
