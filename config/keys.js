18fc7ab4fa28bb1ebb1b62a6cd0cccab10ebad90
Aryan Sharma <aryan@Aryans-MacBook-Pro.local>
2021-08-08 21:23:31 +0530

    removed keys.js from .gitignore

---
+++ config/keys.js
@@ -0,0 +1,10 @@
+module.exports = {
+  googleClientID:
+    "473512430308-27u7jb451qon910o9ptskmfhksvobf51.apps.googleusercontent.com",
+  googleClientSecret: "CEdMDOBlK3AiPwrozbHDKHJV",
+  mongoURI: "mongodb+srv://aryan1939:admin@cluster0.rp7ef.mongodb.net/test",
+  cookieKey: "kasjdfkjahdsguiadgnmadhgdjfhddhueaqdqnuasdhalaiwwhe",
+  postmanClientID:
+    "473512430308-bl0t70383e4f1tb3brbv8ssei1jl0qjq.apps.googleusercontent.com",
+  postmanClientSecret: "1hrKEsqW8cJWkRNlI9cv-o8M",
+};
